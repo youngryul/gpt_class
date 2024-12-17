f099f00950bab71fe51470f44cddd4e1eb61c85c
PNS212624\Hansol <yrlee00@hansol.com>
2024-12-17 16:18:07 +0900

    [12/17] streamlit 세팅 및 초안 작성

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-import tiktoken
+import streamlit as st
+from langchain.prompts import PromptTemplate
 
-print(tiktoken)
+st.title("Hello world!")
+
+st.write(PromptTemplate) #PromptTemplate에 있는 property를 다 볼 수 있다.
+
+st.selectbox(
+    "Choose your model",
+    (
+        "GPT-3",
+        "GPT-4"
+    )
+)
+
+# 데이터가 변경되면 모든 페이지의 내용이 새로고침 된다.
