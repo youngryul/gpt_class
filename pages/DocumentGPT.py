cedb488fdb43325ffbea8333ca947435fe67b939
Leeyoungryul <lekjkj@naver.com>
2024-12-18 23:55:49 +0900

    fix: key 추가

--- pages/DocumentGPT.py
+++ pages/DocumentGPT.py
@@ -55,7 +55,7 @@ def embed_file(file):
     )
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
