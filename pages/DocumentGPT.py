334519c071a37181075de32d31fa6036629a931a
Leeyoungryul <lekjkj@naver.com>
2024-12-18 23:11:08 +0900

    fix: import 오류 수정

--- pages/DocumentGPT.py
+++ pages/DocumentGPT.py
@@ -1,10 +1,11 @@
 from langchain.prompts import ChatPromptTemplate
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.embeddings import CacheBackedEmbeddings
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores.faiss import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
