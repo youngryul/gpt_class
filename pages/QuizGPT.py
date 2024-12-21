a47bf645760e73aa6f8f76b517bd31700ed90781
Leeyoungryul <lekjkj@naver.com>
2024-12-21 17:48:04 +0900

    feat: quizGPT 생성

--- pages/QuizGPT.py
+++ pages/QuizGPT.py
@@ -1,8 +1,21 @@
 import streamlit as st
+import json
+from operator import rshift
 from langchain.retrievers import WikipediaRetriever
-from langchain.document_loaders import UnstructuredFileLoader
+from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.schema import BaseOutputParser, output_parser
+
+class JsonOutputParser(BaseOutputParser):
+
+    def parse(self, text):
+        text = text.replace("```", "").replace("json", "")
+        return json.loads(text)
+    
+output_parser = JsonOutputParser()
 
 st.set_page_config(
     page_title="QuizGPT",
@@ -14,7 +27,178 @@ st.title("QuizGPT")
 llm = ChatOpenAI(
     temperature=0.1,
     model="gpt-3.5-turbo-1106",
+    streaming=True,
+    callbacks=[
+        StreamingStdOutCallbackHandler()
+    ]
 )
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+    
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+        You are a helpful assistant that is role playing as a teacher.
+            
+        Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+        
+        Each question should have 4 answers, three of them must be incorrect and one should be correct.
+            
+        Use (o) to signal the correct answer.
+            
+        Question examples:
+            
+        Question: What is the color of the ocean?
+        Answers: Red|Yellow|Green|Blue(o)
+            
+        Question: What is the capital or Georgia?
+        Answers: Baku|Tbilisi(o)|Manila|Beirut
+            
+        Question: When was Avatar released?
+        Answers: 2007|2001|2009(o)|1998
+            
+        Question: Who was Julius Caesar?
+        Answers: A Roman Emperor(o)|Painter|Actor|Model
+            
+        Your turn!
+            
+        Context: {context}
+        """,
+        )
+    ]
+)
+
+questions_chain = {"context": format_docs} | questions_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a powerful formatting algorithm.
+     
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+     
+    Example Input:
+
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+    
+     
+    Example Output:
+     
+    ```json
+    {{ "questions": [
+            {{
+                "question": "What is the color of the ocean?",
+                "answers": [
+                        {{
+                            "answer": "Red",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Yellow",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Green",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Blue",
+                            "correct": true
+                        }},
+                ]
+            }},
+                        {{
+                "question": "What is the capital or Georgia?",
+                "answers": [
+                        {{
+                            "answer": "Baku",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Tbilisi",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Manila",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Beirut",
+                            "correct": false
+                        }},
+                ]
+            }},
+                        {{
+                "question": "When was Avatar released?",
+                "answers": [
+                        {{
+                            "answer": "2007",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2001",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2009",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "1998",
+                            "correct": false
+                        }},
+                ]
+            }},
+            {{
+                "question": "Who was Julius Caesar?",
+                "answers": [
+                        {{
+                            "answer": "A Roman Emperor",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Painter",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Actor",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Model",
+                            "correct": false
+                        }},
+                ]
+            }}
+        ]
+     }}
+    ```
+    Your turn!
+
+    Questions: {context}
+
+""",
+        )
+    ]
+)
+
+formatting_chain = formatting_prompt | llm
 
 
 @st.cache_data(show_spinner="Loading file...")
@@ -31,6 +215,18 @@ def split_file(file):
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     return docs
+
+@st.cache_data(show_spinner="Making quiz....")
+def run_quiz_chain(_docs, topic):  # 서명을 만들지 않게 해줌 (streamlit 한정문제)
+    chain = {"context" : questions_chain} | formatting_chain | output_parser
+    return chain.invoke(_docs)
+
+@st.cache_data(show_spinner="Searching Wikipedia...")
+def wiki_search(term):
+    retriever = WikipediaRetriever(top_k_results=5)
+    docs = retriever.get_relevant_documents(topic)
+    return docs
+    
 
 with st.sidebar:
     docs = None
@@ -52,9 +248,8 @@ with st.sidebar:
     else:
         topic = st.text_input("Search Wikipedia...")
         if topic:
-            retriever = WikipediaRetriever(top_k_results=5)
-            with st.status("Searching Wikipedia..."):
-                docs = retriever.get_relevant_documents(topic)
+            docs = wiki_search(topic)
+         
 
 
 if not docs:
@@ -68,4 +263,18 @@ if not docs:
     """
     )
 else:
-    st.write(docs)
+  response = run_quiz_chain(docs, topic if topic else file.name)
+  with st.form("questions_form"):
+    for idx, question in enumerate(response["questions"]):
+        st.write(question["question"])
+        value = st.radio(
+            f"Select an option {idx}.",
+            [answer["answer"] for answer in question["answers"]],
+            key=f"{idx}_radio",
+            index=None
+        )
+        if {"answer": value, "correct": True} in question["answers"]:
+            st.success("Correct!")
+        elif value is not None:
+            st.error("Wrong!")
+    button = st.form_submit_button()
